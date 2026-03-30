30164f6194a90f8011d5f2433c3181a296926b10
DeveloperYanRex <yandelpomacarbajal043@gmail.com>
2026-03-29 22:19:15 -0500

    actualizaciones de layouts

--- app/src/main/java/com/articloud/ui/ProfileFragment.kt
+++ app/src/main/java/com/articloud/ui/ProfileFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.articloud.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,43 +20,35 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Referencias
+        val txtNombre = view.findViewById<TextView>(R.id.txtNombre)
+        val txtEmail = view.findViewById<TextView>(R.id.txtEmail)
+        val btnEditar = view.findViewById<Button>(R.id.btnEditar)
+        val btnLogout = view.findViewById<Button>(R.id.btnLogout)
+
+        // Datos de prueba (luego lo conectas a login)
+        txtNombre.text = "Yeff Yandel"
+        txtEmail.text = "yeff@email.com"
+
+        // Eventos
+        btnEditar.setOnClickListener {
+            // Aquí puedes abrir otra pantalla
+        }
+
+        btnLogout.setOnClickListener {
+            // Aquí cerrar sesión
+            txtNombre.text = "Usuario Invitado"
+            txtEmail.text = "correo@ejemplo.com"
+        }
     }
 }
